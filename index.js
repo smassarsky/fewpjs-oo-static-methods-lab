36f09d73db0683c04f36ff3d8d0cc65ee47731b0
smassarsky <seth.massarsky@gmail.com>
2021-01-20 16:11:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(str) {
+    let skipWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(" ").map((word, index) => {
+      if (index === 0 || skipWords.indexOf(word) == -1) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(" ")
+  }
+
 }
